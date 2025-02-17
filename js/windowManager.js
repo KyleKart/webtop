8e0585f469beafab995f34c242a134c6439da31a
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-02-16 23:49:57 -0400

    lollll

--- js/windowManager.js
+++ js/windowManager.js
@@ -62,7 +62,7 @@ export class WindowManager {
     // Setup event listeners
     this.setupWindowEvents(id, windowEl, header);
     this.setupTaskButtonEvents(id, taskButton);
-    this.setupWindowControls(id, closeBtn, maximizeBtn, minimizeBtn);
+    this.setupWindowControls(id, closeBtn, minimizeBtn, maximizeBtn);
     
     this.activateWindow(id);
     
