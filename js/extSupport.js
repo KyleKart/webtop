cf198ee0115a998914656ee362e7e2b0e0ac651a
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-12-31 08:24:50 -0400

    rtrt

--- js/extSupport.js
+++ js/extSupport.js
@@ -1,5 +1,2 @@
 import { WindowManager } from 'https://kylekart.github.io/webtop/js/windowManager.js';
 window.windowManager = new WindowManager();
-
-import { Accent } from 'https://kylekart.github.io/webtop/js/main.js';
-window.getAccent = new Accent();
